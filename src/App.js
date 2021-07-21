0ff1a8c115de0e6006176872e3610f02fc386914
Geon <akrmak12@naver.com>
2021-07-21 16:58:10 +0900

    Start

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App(){
+ 
+  let [글제목, 글제목변경] = useState( ['남자 코트 추천', '강남 우동 맛집'] );
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[0] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+    </div>
+  )
+}
+
+export default App;
